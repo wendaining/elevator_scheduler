4536e6aca1344ecf5076508510db902e307fd0eb
wendaining <wendaining@foxmail.com>
2026-05-12 09:44:35 +0800

    feat: frontend: 搭建 Vue 3 + Element Plus 工程

---
+++ web/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app-root">
+    <h1>电梯调度系统</h1>
+    <p>Vue 3 + Element Plus 已就绪</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #app {
+  height: 100%;
+  width: 100%;
+}
+
+#app-root {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: 'PingFang SC', 'Microsoft YaHei', sans-serif;
+  color: #333;
+}
+</style>
